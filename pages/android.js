e3beda77a7ca27069b550d053a96799bae5ed2e0
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-09-30 01:20:47 +0700

    Update android.js

--- pages/android.js
+++ pages/android.js
@@ -1,25 +1,19 @@
-export async function getServerSideProps(context) {
-  const accept = context.req.headers['accept'] || '';
-  
-  if (!accept.includes('text/html')) {
-    context.res.setHeader('Content-Type', 'text/plain');
-    context.res.end(
+export async function getServerSideProps({ res }) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end(
 `128.199.212.210 growtopia1.com
 128.199.212.210 growtopia2.com
 128.199.212.210 www.growtopia1.com
 128.199.212.210 www.growtopia2.com`
-    );
-    return { props: {} };
-  }
-
+  );
   return { props: {} };
 }
 
-export default function Home() {
+export default function Android() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>🌐</h1>
-      <p>Garuda Host</p>
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>🤖 Hosts File</h1>
+      <p>Gunakan URL ini langsung di Hosts Go / PowerTunnel.</p>
     </div>
   );
 }
