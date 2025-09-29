4671a6b6e05ad29bb4040feb31715ca05cf5db83
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-09-30 01:02:45 +0700

    Create ios.js

---
+++ pages/ios.js
@@ -0,0 +1,30 @@
+export async function getServerSideProps(context) {
+  const accept = context.req.headers['accept'] || '';
+  
+  if (!accept.includes('text/html')) {
+    context.res.setHeader('Content-Type', 'text/plain');
+    context.res.end(
+`[General]
+
+[Rule]
+FINAL,DIRECT
+
+[Host]
+128.199.212.210 www.growtopia1.com
+128.199.212.210 www.growtopia2.com`
+    );
+    return { props: {} };
+  }
+
+  return { props: {} };
+}
+
+export default function IOS() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>📱 iOS Config Page</h1>
+      <p>Halaman ini muncul kalau dibuka lewat browser.</p>
+    </div>
+  );
+}
+ 
